9fa41213502864dcf1764723ab3b85c379e311fc
kweall <kweall@mail.ru>
2026-03-02 23:14:05 +0300

    ci

--- cmd/loglint/main.go
+++ cmd/loglint/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"github.com/kweall/loglint/analyzer"
-	"golang.org/x/tools/go/analysis/singlechecker"
+	"golang.org/x/tools/go/analysis/unitchecker"
 )
 
 func main() {
-	singlechecker.Main(analyzer.Analyzer)
+	unitchecker.Main(analyzer.Analyzer)
 }
